91b90fb2720d7ddbca2a44982d0dfd9e518cbdc7
Palak <palakchiraniya03>
2026-01-08 19:38:52 +0530

    i added autocomplete logic using dfs technique

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
 class TrieNode{
@@ -34,12 +35,47 @@ class Trie{
     }
     curr->isEnd=true;
   }
+
+  void dfs(TrieNode* node,string prefix,vector<string>&a result){
+    if (node->isEnd){
+      result.push_back(prefix);
+    }
+
+    for(int i=0;i<26;i++){
+        if (node->children[i]!= NULL){
+          char nextChar=i + 'a';
+          dfs(node->children[i], prefix + nextChar,result);
+        }
+    }
+  }
+
+  
+  vector<string> autocomplete(string prefix){
+    TrieNode* curr=root;
+    vector<string> result;
+
+    for (char c:prefix) {
+        int index = c-'a';
+        if (curr->children[index] == NULL)
+            return result;
+        curr = curr->children[index];
+    }
+
+    dfs(curr, prefix, result);
+    return result;
+  }
 };
 
 int main(){
   Trie trie;
   trie.insert("apple");
   trie.insert("app");
-  cout<<"inserted some words in the trie"<<endl;
+  trie.insert("application");
+  trie.insert("banana");
+  vector<string> words=trie.autocomplete("app");
+
+  for(string w:words){
+    cout<<w<<endl;
+  }
   return 0;
 }
